486a833f5076ab94518c71611c92b380b32fa49c
Raphael Canestrale <92121549+FaelzN@users.noreply.github.com>
2025-04-23 20:37:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.texto-multiplo', {
+    strings: ['Web-design', 'Desenvolvedor FullStack'],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
